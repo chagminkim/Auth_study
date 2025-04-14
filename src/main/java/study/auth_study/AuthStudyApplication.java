0ceb9ffe817cd5ec065a9ac47e828e891ce5f5b4
changminkim <cmk5724@naver.com>
2025-04-14 18:17:21 +0900

    project init

---
+++ src/main/java/study/auth_study/AuthStudyApplication.java
@@ -0,0 +1,13 @@
+package study.auth_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/auth_study/AuthStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.auth_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

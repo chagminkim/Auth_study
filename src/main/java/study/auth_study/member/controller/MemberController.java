0d5cde3e4f0d4ab5af00469c943db459c16171a1
changminkim <cmk5724@naver.com>
2025-04-17 00:23:09 +0900

    MemberController 정리

--- src/main/java/study/auth_study/member/controller/MemberController.java
+++ src/main/java/study/auth_study/member/controller/MemberController.java
@@ -2,15 +2,15 @@ package study.auth_study.member.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import study.auth_study.member.repository.MemberRepository;
-import study.auth_study.member.service.MemberService;
 
 @RestController
-
 public class MemberController {
 
     private final MemberRepository memberRepository;
 
-    public MemberController(MemberRepository memberRepository) {this.memberRepository = memberRepository;}
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
 }
 
